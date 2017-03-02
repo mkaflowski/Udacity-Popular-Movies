91a222753898417d8bde6987b5a0a3533620c8c7
Mateusz <mkaflowski@gmail.com>
2017-03-02 17:06:07 +0100

    menu

--- app/src/main/java/pl/mateuszkaflowski/udacity_popular_movies/MainActivity.java
+++ app/src/main/java/pl/mateuszkaflowski/udacity_popular_movies/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.socks.library.KLog;
@@ -18,9 +20,12 @@ import pl.mateuszkaflowski.udacity_popular_movies.moviedata.MovieCollector;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String POPULAR_IDENTIFIER = "POPULAR";
+    public static final String TOP_RATED_IDENTIFIER = "TOP_RATED";
     private RecyclerView recyclerView;
     private MovieAdapter movieAdapter;
     private List<Movie> movieList;
+    private CollectMovieInfoAsyncTask collectTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +40,52 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(movieAdapter);
 
-        CollectMovieInfoAsyncTask collectTask = new CollectMovieInfoAsyncTask();
-        collectTask.execute();
+        collectTask = new CollectMovieInfoAsyncTask();
+        collectTask.execute(POPULAR_IDENTIFIER);
     }
 
-    private class CollectMovieInfoAsyncTask extends AsyncTask<Void, Void, List<Movie>> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuPopular:
+                collectTask = new CollectMovieInfoAsyncTask();
+                collectTask.execute(POPULAR_IDENTIFIER);
+                break;
+            case R.id.menuTopRated:
+                collectTask = new CollectMovieInfoAsyncTask();
+                collectTask.execute(TOP_RATED_IDENTIFIER);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class CollectMovieInfoAsyncTask extends AsyncTask<String, Void, List<Movie>> {
 
         @Override
-        protected List<Movie> doInBackground(Void... voids) {
-            try {
-                List<Movie> movies = MovieCollector.getPopularMovies(CommonConstants.API_KEY);
-                return movies;
-            } catch (Throwable throwable) {
-                throwable.printStackTrace();
+        protected List<Movie> doInBackground(String... strings) {
+            if (strings[0].equals(POPULAR_IDENTIFIER)) {
+                try {
+                    List<Movie> movies = MovieCollector.getPopularMovies(CommonConstants.API_KEY);
+                    return movies;
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+
+            if (strings[0].equals(TOP_RATED_IDENTIFIER)) {
+                try {
+                    List<Movie> movies = MovieCollector.getTopRatedMovies(CommonConstants.API_KEY);
+                    return movies;
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
             }
             return null;
         }
